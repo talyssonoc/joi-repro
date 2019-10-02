fc353730969a9eb7013d79d2d86d392bdba10b94
Eran Hammer <eran@hammer.io>
2019-10-02 09:42:05 -0700

    Update User.js

--- src/User.js
+++ src/User.js
@@ -10,47 +10,17 @@ class User {
   validate() {
     const Friend = require('./Friend');
 
-    const schema = joi.object().keys({
-      ...User.schema,
-
-      /*
-
-        I don't know how to make the friends schema here
-        in a way that it also validates the friends inside
-        the user of one of the friends (I know it sounds confusing)
-        example scenario: I wanna be able to validate this:
-
-        {
-          name: 'ABC',
-          friends: [
-            {
-              since: new Date(),
-              user: {
-                name: 'CBA',
-                friends: [
-                  {
-                    since: new Date(),
-                    user: {
-                      name: 'AAA',
-                      friends: []
-                    }
-                  }
-                ]
-              }
-            }
-          ]
-        }
-
-      */
-      friends: joi.array().items(Friend.schema)
-    });
+    const schema = User.schema
+      .shared(Friend.schema);
 
     return schema.validate(this);
   }
 }
 
-User.schema = {
-  name: joi.string().min(2)
-};
+User.schema = Joi.object({
+  name: joi.string().min(2),
+  friends: joi.array().items(Joi.link('#friend'))
+})
+  .id('user');
 
 module.exports = User;
